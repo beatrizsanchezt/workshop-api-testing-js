d46a7f1186fe6226135c60ba2c434f507a8b5be7
12358-lab <12358-lab@protonmail.com>
2020-01-04 17:04:16 +0000

    Adding eslint

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,21 +2,20 @@ const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
-
   it('Consume DELETE Service', async () => {
     const data = {
       name: 'Doe',
       age: '35',
       city: 'Nashville'
-    };    
+    };
     const response = await agent.delete('https://httpbin.org/delete').send(data);
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.json).to.eql(data);  
+    expect(response.body.json).to.eql(data);
   });
-  
+
   it('Consume GET Service', async () => {
     const response = await agent.get('https://httpbin.org/ip');
     expect(response.status).to.equal(statusCode.OK);
@@ -46,7 +45,7 @@ describe('First Api Tests', () => {
       name: 'Doe',
       age: '35',
       city: 'Nashville'
-    };    
+    };
     const response = await agent.patch('https://httpbin.org/patch').send(data);
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.json).to.eql(data);
@@ -57,10 +56,9 @@ describe('First Api Tests', () => {
       name: 'Doe',
       age: '35',
       city: 'Nashville'
-    };    
+    };
     const response = await agent.put('https://httpbin.org/put').send(data);
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body.json).to.eql(data);
   });
-
 });
